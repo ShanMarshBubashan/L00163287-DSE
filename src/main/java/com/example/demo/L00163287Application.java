59c479b5fb4bd26a8ced0c7037a15c31711b52fe
Bubashan <bubashanb1@gmail.com>
2021-10-10 17:12:36 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/L00163287Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L00163287Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L00163287Application.class, args);
+    }
+
+}
